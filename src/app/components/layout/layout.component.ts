1676de5b0897d4cbd924b10903b201518b199d21
GopinathJanakiraman <gopinath.j.88@gmail.com>
2024-03-16 18:51:37 +0530

    carecell cover print

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,23 +1,33 @@
-import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import { Router } from '@angular/router';
+import { AfterViewInit, ChangeDetectorRef, Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
-import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent {
+export class LayoutComponent implements AfterViewInit {
   menuToggle: boolean = false;
   year: number = new Date().getFullYear();
+  currentUrl!: string;
   constructor(
     private router: Router,
     private cookie: CookieService,
-    private formBuilder: FormBuilder,
-    private toastr: ToastrService
-  ) { }
+    private cdr: ChangeDetectorRef
+  ) {
+    this.router.events.subscribe((e) => {
+      if (e instanceof NavigationEnd) {
+        this.currentUrl = e.url;
+      }
+    })
+
+
+  }
+
+  ngAfterViewInit(): void {
+    this.cdr.detectChanges();
+  }
   logout() {
     this.menuToggle = !this.menuToggle;
     this.cookie.delete('login', '/');
