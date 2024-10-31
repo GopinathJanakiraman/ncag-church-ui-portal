3a9123949db79cab57d4a30a24ebd2ffcaf8078b
GopinathJanakiraman <gopinath.j.88@gmail.com>
2024-10-31 13:37:28 +0530

    carecell search fix

--- src/app/components/carecell-cover/carecell-cover.component.ts
+++ src/app/components/carecell-cover/carecell-cover.component.ts
@@ -15,14 +15,28 @@ export class CarecellCoverComponent implements OnInit {
   pagesArray: number[] = [];
   carecellList: any = [];
   areaId: number = 0;
+  regionId: any;
   week: string = 'I';
   month: string = new Date().toLocaleString('default', { month: 'short' });
   year: number = new Date().getFullYear();
   weeks: string[] = ['I', 'II', 'III', 'IV', 'V', 'VI'];
   months: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  regionList: any = [];
   constructor(private service: NcagService, private toaster: ToastrService) { }
   ngOnInit(): void {
     this.getAllStaticContent();
+  }
+  getAllRegionList() {
+    this.service
+      .commonGETCall(AppSettingsModule.regionService)
+      .subscribe((dataset: any) => {
+        this.regionList = dataset.content;
+        this.regionList.unshift({ regionId: 0, regionName: "Select Region", areaList: [] })
+        console.log(this.regionList)
+        this.search();
+      }, (err: any) => {
+        this.toaster.error("Something went wrong", "Error !");
+      });
   }
   getAllStaticContent() {
     this.service
@@ -33,7 +47,8 @@ export class CarecellCoverComponent implements OnInit {
           id: 0,
           name: "Select Area"
         })
-        this.getAllList(this.pageNo);
+
+        this.getAllRegionList();
 
       }, (err: any) => {
         this.toaster.error("Something went wrong", "Error !");
@@ -58,9 +73,9 @@ export class CarecellCoverComponent implements OnInit {
   }
   search() {
     let searchParam = {
-      areaId: this.areaId || 0,
+      areaList: this.areaId ? this.areaId : this.regionId?.areaList.map((a: any) => a.id),
       page: 0,
-      size: 10
+      size: 100000
     }
     this.service
       .commonPOSTCall(AppSettingsModule.carecellService + '/search', searchParam)
